a2e86ff22e8afa4f94dc5e3daa9a5b088b039fc8
holtye <holtye@qq.com>
2026-06-30 18:25:44 +0800

    Use site name in Toutiao feed sidebar

--- resources/views/theme/toutiao-news-20260426/partials/sidebar.blade.php
+++ resources/views/theme/toutiao-news-20260426/partials/sidebar.blade.php
@@ -10,7 +10,7 @@
 <aside class="tt-sidebar">
     @if(!empty($showFeedPanel))
         <section class="tt-panel tt-feed-panel">
-            <div class="tt-page-kicker">GEOFlow Feed</div>
+            <div class="tt-page-kicker">{{ $siteName }}</div>
             <h2 class="tt-feed-panel-title">{{ $feedTitle }}</h2>
             @if($feedDescription !== '')
                 <p class="tt-feed-panel-desc">{{ $feedDescription }}</p>
